8cf1eb295a7e61b7c24e50922518d37fe135e9c8
DonChrapek <daniel.chrapczynski@gmail.com>
2017-06-25 13:00:59 +0200

    Added simple styles to Blog Component

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,15 +1,67 @@
 import '../styles/Blog.css';
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Blog = () => {
     return (
       <section id='blog'>
-        <div className='section-title centered'>
-          <h3>Blog</h3>
+        <div className='centered section-title'>
+          <h3>Recent articles</h3>
         </div>
-        <article id='' className='recent-article'>
-          
+        <article className='half-wide'>
+          <div className='article-img'>
+                <Link to='/blog'><img src='../images/blog/article1.jpg' alt='Lorem ipsum' /></Link>
+              </div>
+              <h4 className='article-title'>Lorem ipsum</h4>
+              <span className='article-content'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </span>
+        </article>
+        <article className='half-wide'>
+          <div className='article-img'>
+                <Link to='/blog'><img src='../images/blog/article1.jpg' alt='Lorem ipsum' /></Link>
+              </div>
+              <h4 className='article-title'>Lorem ipsum</h4>
+              <span className='article-content'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </span>
+        </article>
+        <article className='half-wide'>
+          <div className='article-img'>
+                <Link to='/blog'><img src='../images/blog/article1.jpg' alt='Lorem ipsum' /></Link>
+              </div>
+              <h4 className='article-title'>Lorem ipsum</h4>
+              <span className='article-content'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </span>
+        </article>
+        <article className='half-wide'>
+          <div className='article-img'>
+                <Link to='/blog'><img src='../images/blog/article1.jpg' alt='Lorem ipsum' /></Link>
+              </div>
+              <h4 className='article-title'>Lorem ipsum</h4>
+              <span className='article-content'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </span>
+        </article>
+        <article className='half-wide'>
+          <div className='article-img'>
+                <Link to='/blog'><img src='../images/blog/article1.jpg' alt='Lorem ipsum' /></Link>
+              </div>
+              <h4 className='article-title'>Lorem ipsum</h4>
+              <span className='article-content'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </span>
+        </article>
+        <article className='half-wide'>
+          <div className='article-img'>
+                <Link to='/blog'><img src='../images/blog/article1.jpg' alt='Lorem ipsum' /></Link>
+              </div>
+              <h4 className='article-title'>Lorem ipsum</h4>
+              <span className='article-content'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </span>
         </article>
       </section>
     );
